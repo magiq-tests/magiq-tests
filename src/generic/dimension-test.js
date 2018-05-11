fe90a7ed3556cfe137bfbce14edfe6b284c17909
parashar.sangle@gramener.com <parashar.sangle@gramener.com>
2018-05-11 15:52:45 +0530

    FIX svg dimension check

--- src/generic/dimension-test.js
+++ src/generic/dimension-test.js
@@ -5,8 +5,8 @@ export function svgDimensionCheck(selector, height, width) {
   width = width || 500;
   
   test('check SVG height and width', function (assert) {
-    assert.equal(height, parseInt($(selector).attr('height')), "height")
-    assert.equal(width, parseInt($(selector).attr('width')), "width")
+    assert.equal(height, parseInt($(selector).css('height')), "height")
+    assert.equal(width, parseInt($(selector).css('width')), "width")
     assert.end();
   });
 
